88657c85cf6eca85cf598638191d49288f690d34
rahul138 <rahul.kumar17407@marwadieducation.edu.in>
2021-09-15 23:29:22 +0530

    5 new program is added

---
+++ ForLoop/palindromicPattern.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << count;
+            count--;
+        }
+        count++;
+        cout << "\n";
+    }
+}
